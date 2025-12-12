490b0caf0134a63b2162c8d6d83f3e7da88f7f8b
Ilias-Iason Papanikolaou-Polymenidis <iaswn.papanikolaou@hotmail.com>
2025-12-12 19:46:18 +0200

    tempalte api is created

--- server.py
+++ server.py
@@ -3,33 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 contactsList = [
-    {
-        id:'1',
-        name:'jhon'
-        number:'6985698458'
-    },
-    {
-        id:'2',
-        name:'adam'
-        number:'6985688458'
-    },
-    {
-        id:'3',
-        name:'peter'
-        number:'6934698458'
-    },
-    {
-        id:'4',
-        name:'jason'
-        number:'6968698458'
-    },
+    {'id':'1','name':'john','number':'6985699842'},
 ]
+
 
 @app.route('/')
 def homepage():
     return '<h1>Hello there!</h1>'
 
 @app.get('/contacts')
-def list_contacts:
+def list_contacts():
+    return contactsList
 
-    
+app.run(debug=True)
+

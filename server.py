9880c9d4bf651113aa407c08017706eb3eb39d9b
Ilias-Iason Papanikolaou-Polymenidis <iaswn.papanikolaou@hotmail.com>
2025-12-15 20:07:47 +0200

    Configured standardized api response

--- server.py
+++ server.py
@@ -1,127 +1,131 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 contacts = [
-    {'id':1,'name':'john','number':'6985699842'},
-    {'id':2,'name':'adam','number':'6985239842'},
-    {'id':3,'name':'peter','number':'6985645842'},
+    {"id": 1, "name": "john", "number": "6985699842"},
+    {"id": 2, "name": "adam", "number": "6985239842"},
+    {"id": 3, "name": "peter", "number": "6985645842"},
 ]
 
-#GET    - /contacts - list contacts
-#GET    - /contacts/<id> - list specific contact
-#POST   - /contacts - add contact
-#PUT    - /contacts/<id> - edit contact
-#DELETE - /contacts/<id> - delete contact
+# GET    - /contacts - list contacts
+# GET    - /contacts/<id> - list specific contact
+# POST   - /contacts - add contact
+# PUT    - /contacts/<id> - edit contact
+# DELETE - /contacts/<id> - delete contact
 
-def successResponse(sucessMessage = None,data = None):
-    successMsg = {
-        'success': True,
-        'message':sucessMessage,
-        'data': data,
-    }
 
-    return successMsg
+def createResponse(
+    data: list | dict | None = None,
+    success: bool = True,
+    errMsg: str | None = None,
+    errCode: int | None = None,
+):
 
-def errorResponse(errorMessage = 'Unexpected error',errorCode = 400):
-    errorMsg = {
-        'data': None,
-        'error': {
-            'message': errorMessage,
-            'code': errorCode
+    if success:
+        response = {"success": True, "data": data, "error": None}
+    else:
+        response = {
+            "success": False,
+            "data": None,
+            "error": {"message": errMsg, "code": errCode},
         }
-    }
+    
+    return jsonify(response)
 
-    return errorMsg
 
-
-@app.route('/')
+@app.route("/")
 def homepage():
-    return '<h1>Hello there!</h1>'
+    return "<h1>Hello there!</h1>"
 
-@app.get('/contacts')
+
+@app.get("/contacts")
 def list_contacts():
-    return jsonify(successResponse(contacts))
+    return (createResponse(data=contacts), 200)
 
-@app.get('/contacts/<id>')
+
+@app.get("/contacts/<int:id>")
 def list_contact(id):
 
-    if not id.isdigit():
-        return (jsonify(errorResponse('ResourceNotFound',404)),404)
-    
     for contact in contacts:
-        if contact['id'] == int(id):
-            return jsonify(successResponse(contact))
-        
-    return (jsonify(
-        errorResponse('ResourceNotFound',404)),
-        404
-        )
+        if contact["id"] == id:
+            return (createResponse(data=contact), 200)
 
-@app.post('/contacts')
+    return (
+        createResponse(success=False, errMsg="SourceNotFound", errCode=404),
+        404,
+    )
+
+
+@app.post("/contacts")
 def add_contact():
     payload = request.get_json(silent=True)
 
     if not payload:
-        return (jsonify(
-        errorResponse('InvalidPayload',400)),
-        400
+        return (
+            createResponse(success=False, errMsg="InvalidPayload", errCode=400),
+            400,
         )
-    
-    contactName = payload.get('name')
-    contactNumber = payload.get('number')
+
+    contactName = payload.get("name")
+    contactNumber = payload.get("number")
 
     if contactName and contactNumber:
         newContact = {
-            'id': max(int(c['id']) for c in contacts) + 1 if contacts else 1,
-            'name': contactName,
-            'number': contactNumber
+            "id": max(int(c["id"]) for c in contacts) + 1 if contacts else 1,
+            "name": contactName,
+            "number": contactNumber,
         }
 
         contacts.append(newContact)
 
-        return jsonify(successResponse('Contact added to the catalog',newContact)),201
-    
-    return (jsonify(
-        errorResponse('InvalidSchema',400)),
-        400
-        )
+        return (createResponse(data=newContact), 201)
 
-@app.put('/contacts/<id>')
+    return (
+        createResponse(success=False, errMsg="InvalidPayload", errCode=400),
+        400,
+    )
+
+
+@app.put("/contacts/<id>")
 def edit_contact(id):
-
-    if not id.isdigit():
-        return (jsonify(errorResponse('ResourceNotFound',404)),404)
 
     payload = request.get_json(silent=True)
 
     if not payload:
-        return (jsonify(
-        errorResponse('UnsupportedPayload',400)),
-        400
+        return (
+            createResponse(success=False, errMsg="InvalidPayload", errCode=400),
+            400,
         )
 
-    contactName = payload.get('name')
-    contactNumber = payload.get('number')
+    contactName = payload.get("name")
+    contactNumber = payload.get("number")
 
     for contact in contacts:
-        if contact['id'] == int(id):
-            contact['name'] = contactName if contactName else contact['name']
-            contact['number'] = contactNumber if contactNumber else contact['number']
-            
-            return jsonify(successResponse('Contact changed',contact)),201
-        
-    return (jsonify(errorResponse('ResourceNotFound',404)),404)
+        if str(contact["id"]) == id:
+            contact["name"] = contactName if contactName else contact["name"]
+            contact["number"] = contactNumber if contactNumber else contact["number"]
 
-@app.delete('/contacts/<id>')
+            return (createResponse(data=contact), 200)
+
+    return (
+        createResponse(success=False, errMsg="SourceNotFound", errCode=404),
+        404,
+    )
+
+
+@app.delete("/contacts/<id>")
 def delete_contact(id):
 
     for contact in contacts:
-        if contact['id'] == int(id):
+        if str(contact["id"]) == id:
             contacts.remove(contact)
-            return jsonify(successResponse('Contact deleted',contact))
-        
-    return (jsonify(errorResponse('ResourceNotFound',404)),404)
+            return ("", 204)
+
+    return (
+        createResponse(success=False, errMsg="SourceNotFound", errCode=404),
+        404,
+    )
+
 
 app.run(debug=True)
-

135495dd637c5591b7f13c0fe9341dc062e842d4
Ilias-Iason Papanikolaou-Polymenidis <iaswn.papanikolaou@hotmail.com>
2025-12-19 14:49:26 +0200

    Moved return method outside of context managers. in database.py

--- database.py
+++ database.py
@@ -17,6 +17,8 @@ def init_db():
         """)
         conn.commit()
 
+    return True
+
 def get_contacts(page,limit):
 
     offset_rows = (page - 1) * limit
@@ -25,14 +27,16 @@ def get_contacts(page,limit):
         cursor = conn.cursor()
         cursor.execute('SELECT id, name, number FROM contacts ORDER BY id ASC LIMIT ? OFFSET ?',(limit,offset_rows,))
         rows = cursor.fetchall()
-        return map_rows_to_contacts(rows)
+    
+    return map_rows_to_contacts(rows)
 
 def count_contacts():
 
     with sqlite3.connect(DATABASE_FILENAME) as conn:
         cursor = conn.cursor()
         row = cursor.execute('SELECT COUNT(*) FROM contacts').fetchone()
-        return row[0]
+    
+    return row[0]
 
 def get_contact_by_id(contact_id):
 
@@ -40,7 +44,8 @@ def get_contact_by_id(contact_id):
         cursor = conn.cursor()
         cursor.execute('SELECT id,name,number FROM contacts WHERE id = ?', (contact_id,))
         row = cursor.fetchone()
-        return map_values_to_contact(row) if row else None
+    
+    return map_values_to_contact(row) if row else None
 
 def create_contact(name, number):
 
@@ -48,7 +53,8 @@ def create_contact(name, number):
         cursor = conn.cursor()
         cursor.execute('INSERT INTO contacts (name,number) VALUES(?,?)',(name,number,))
         conn.commit()
-        return cursor.lastrowid
+    
+    return cursor.lastrowid
 
 def update_contact(contact_id, name, number):
 
@@ -56,7 +62,8 @@ def update_contact(contact_id, name, number):
         cursor = conn.cursor()
         cursor.execute('UPDATE contacts SET name = ?, number = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ?',(name, number, contact_id,))
         conn.commit()
-        return cursor.rowcount > 0
+    
+    return cursor.rowcount > 0
     
 def remove_contact(contact_id):
 
@@ -64,7 +71,8 @@ def remove_contact(contact_id):
         cursor = conn.cursor()
         cursor.execute('DELETE FROM contacts WHERE id = ?',(contact_id,))
         conn.commit()
-        return cursor.rowcount > 0
+    
+    return cursor.rowcount > 0
 
 def map_values_to_contact(row):
 
